2003bd1121450b74797c4775b0dd7e773f0eb75d
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-01-31 15:55:23 +0100

    WIP create and populate db

---
+++ create_db.sql
@@ -0,0 +1,24 @@
+create database BOOKSTORE
+alter database BOOKSTORE
+set change_tracking = in (
+	change_retention = 2 days, auto_cleanup = on
+);
+
+
+create table BOOKSTORE.BOOKS (
+	ISBN char(13) not null,
+	TITLE varchar(256) not null,
+	AUTHOR varchar(256) not null,
+);
+
+alter table BOOKSTORE.BOOKS
+enable change_tracking
+with (track_columns_updated = on)
+
+
+insert into BOOKSTORE.BOOKS (ISBN, TITLE, AUTHOR) values
+	('9781503261969', 'Emma', 'Jayne Austen'),
+	('9781505255607', 'The Time Machine', 'H. G. Wells'),
+	('9781503379640', 'The Prince', 'Niccolò Machiavelli')
+; 
+
