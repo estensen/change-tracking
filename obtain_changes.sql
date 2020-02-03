c1a60fab88d6533a97e1cfecd49c6550d5941e15
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-02-03 13:54:18 +0100

    Declare vars for change tracking

--- obtain_changes.sql
+++ obtain_changes.sql
@@ -1,3 +1,4 @@
+declare @synchronization_version bigint;
 set @synchronization_version = CHANGE_TRACKING_CURRENT_VERSION();  
 select B.ISBN, B.TITLE, B.AUTHOR
 from BOOKS
@@ -6,7 +7,7 @@ insert into BOOKS (ISBN, TITLE, AUTHOR) values
 	('9781449373320', 'Designing Data-Intensive Applications', 'Martin Kleppmann')
 ;
 
---- Maybe create id instead of using isbn
+declare @last_synchronization_version bigint;
 select B.ISBN, B.TITLE, B.AUTHOR, CT.SYS_CHANGE_OPERATION, CT.SYS_CHANGE_COLUMNS, CT.SYS_CHANGE_CONTEXT
 from BOOKS as B
 right outer join CHANGETABLE(CHANGES BOOKS, @last_synchronization_version) as CT
