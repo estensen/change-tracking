eedc9a19becfb9475ed813de499f8faa552a654d
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-02-03 13:53:17 +0100

    Remove reference to BOOKSTORE

--- obtain_changes.sql
+++ obtain_changes.sql
@@ -1,14 +1,14 @@
 set @synchronization_version = CHANGE_TRACKING_CURRENT_VERSION();  
 select B.ISBN, B.TITLE, B.AUTHOR
-from BOOKSTORE.BOOKS
+from BOOKS
 
-insert into BOOKSTORE.BOOKS (ISBN, TITLE, AUTHOR) values
+insert into BOOKS (ISBN, TITLE, AUTHOR) values
 	('9781449373320', 'Designing Data-Intensive Applications', 'Martin Kleppmann')
 ;
 
 --- Maybe create id instead of using isbn
 select B.ISBN, B.TITLE, B.AUTHOR, CT.SYS_CHANGE_OPERATION, CT.SYS_CHANGE_COLUMNS, CT.SYS_CHANGE_CONTEXT
-from BOOKSTORE.BOOKS as B
-right outer join CHANGETABLE(CHANGES BOOKSTORE.BOOKS, @last_synchronization_version) as CT
+from BOOKS as B
+right outer join CHANGETABLE(CHANGES BOOKS, @last_synchronization_version) as CT
 on B.ID = CT.ID
 
